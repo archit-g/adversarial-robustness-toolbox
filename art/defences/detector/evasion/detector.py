48ce1ad00c9b77ec6855e3e2a1128d612c6ce3da
Tirth Mehta <tirthmehta05@gmail.com>
2022-11-08 20:41:01 -0500

    Stateful Detector Skeleton Running Code with results matched

--- art/defences/detector/evasion/detector.py
+++ art/defences/detector/evasion/detector.py
@@ -1,12 +1,28 @@
-from abc import ABC
+# MIT License
+#
+# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2022
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+from typing import List, Optional, Tuple, Union, TYPE_CHECKING
 import numpy as np
 
-# from typing import Optional, Tuple
-
-# from art.config import ART_NUMPY_DTYPE
-
-# logger = logging.getLogger(__name__)
-
+from abc import ABC
 
 class Detector(ABC):
     # def __init__(self):
@@ -21,7 +37,3 @@ class Detector(ABC):
 
     def detect(self, x: np.ndarray, batch_size: int = 128, **kwargs) -> np.ndarray:
         raise NotImplementedError
-        
-		
-
-
--- art/defences/detector/evasion/model_detector.py
+++ art/defences/detector/evasion/model_detector.py
@@ -1,5 +1,28 @@
-from detector import Detector
+# MIT License
+#
+# Copyright (C) The Adversarial Robustness Toolbox (ART) Authors 2022
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+from typing import List, Optional, Tuple, Union, TYPE_CHECKING
 import numpy as np
+
+from art.defences.detector.evasion.detector import Detector
 
 
 class ModelDetector(Detector):
@@ -8,7 +31,6 @@ class ModelDetector(Detector):
       self.model = model
 
     def _init_encoder(self, weights_path):
-        print("*")
         encoder = self.model
         encoder.load_weights(weights_path, by_name=True)
         self.encoder = encoder
