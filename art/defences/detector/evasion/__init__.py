3f3e784338a355eff52f05392a5434da02f9cd5d
Archit Gajpal <agajpal@cs.stonybrook.edu>
2022-11-24 23:32:34 +0000

    updating init file for evasion directory
    Signed-off-by: Archit Gajpal <agajpal@cs.stonybrook.edu>

--- art/defences/detector/evasion/__init__.py
+++ art/defences/detector/evasion/__init__.py
@@ -3,8 +3,12 @@ Module implementing detector-based defences against evasion attacks.
 """
 from art.defences.detector.evasion import subsetscanning
 
-from art.defences.detector.evasion.detector_old import BinaryInputDetector, BinaryActivationDetector
+from art.defences.detector.evasion.binary_detector import BinaryInputDetector, BinaryActivationDetector
 
 from art.defences.detector.evasion.detector import Detector
 
 from art.defences.detector.evasion.blacklight_detector import BlacklightDetector
+
+from art.defences.detector.evasion.model_detector import ModelDetector
+
+from art.defences.detector.evasion.stateful_defense import StatefulDefense
